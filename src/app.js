d1fb94b51cde72811f372586558a476c3368ffd5
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-04-23 11:54:00 +0200

    Added several functions in app.js
    Added searchCity to handle the submit form;
    Added searchWeather to handle the api call;
    Added updateWeatherInformation to replace the elements in HTML by data from the api call

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-function replaceCity(event) {
-  event.preventDefault();
+function updateWeatherInformation(response) {
   let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchWeather(city) {
+  let apiKey = "0050t012172bb4a92f6b53332o6cb5f4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInformation);
+}
+
+function searchCity(event) {
+  event.preventDefault();
   let cityInput = document.querySelector("#city-search-input");
-  city.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
 
 let form = document.querySelector("form");
-form.addEventListener("submit", replaceCity);
+form.addEventListener("submit", searchCity);
+
+searchWeather("Lelystad");
